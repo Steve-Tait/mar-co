c9208a01db9179d67843002e5dca361c5ac26169
Steve Tait <stephenrichardtait@gmail.com>
2025-03-21 16:35:10 +0000

    fix robots

--- app/robots.ts
+++ app/robots.ts
@@ -10,7 +10,7 @@ export default function robots(): MetadataRoute.Robots {
           }
         : {
             userAgent: '*',
-            disallow: '',
+            disallow: '/',
           },
   };
 }

c3bb15ab619e94d365ff2347a0d519dff42c984e
Steve Tait <stephenrichardtait@gmail.com>
2025-03-21 16:24:55 +0000

    adding logic to robots

--- app/robots.ts
+++ app/robots.ts
@@ -2,9 +2,15 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      disallow: '/',
-    },
+    rules:
+      process.env.VERCEL_ENV === 'production'
+        ? {
+            userAgent: '*',
+            allow: '/',
+          }
+        : {
+            userAgent: '*',
+            disallow: '',
+          },
   };
 }

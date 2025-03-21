fa99064aca0ce9ed14ad6e73ffd753940452bba2
Steve Tait <stephenrichardtait@gmail.com>
2025-03-21 17:01:36 +0000

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+import { getLinks } from '@/lib/storyblok';
+import type { MetadataRoute } from 'next';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const url = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : `https://mar-co.digital`;
+
+  const links = await getLinks();
+
+  if (!links) {
+    return [];
+  }
+  return Object.values(links).map((link) => ({
+    url: `${url}${link.real_path}/`,
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: 1,
+  }));
+}

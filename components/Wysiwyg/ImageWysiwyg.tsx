784866a26abfe10bf88e4520625e0c86dcc05270
Steve Tait <stephenrichardtait@gmail.com>
2026-03-26 22:58:05 +0000

    fix wysiwyg spacing

--- components/Wysiwyg/ImageWysiwyg.tsx
+++ components/Wysiwyg/ImageWysiwyg.tsx
@@ -1,28 +1,33 @@
-import { ImageWysiwygStoryblok } from '@/component-types-sb';
-import Container from '../Shared/Container';
-import { Camera } from 'lucide-react';
+import { ImageWysiwygStoryblok } from "@/component-types-sb";
+import Container from "../Shared/Container";
+import { Camera } from "lucide-react";
+import Image from "next/image";
+import { getImageDimensionsFromUrl } from "@/lib/utils";
 
 const ImageWysiwyg = ({ blok }: { blok: ImageWysiwygStoryblok }) => {
-  const { image, caption } = blok;
-  if (!image) return;
-  return (
-    <div className='theme--light bg-background py-8 text-foreground md:py-12 xl:py-16'>
-      <Container className='theme--light text-center'>
-        <div className='inline-block max-w-full overflow-hidden rounded-xl'>
-          <img
-            className='block'
-            src={image.filename}
-            alt={image.alt || image.title || ''}
-          />
-          {caption && (
-            <div className='flex items-center gap-x-4 bg-purple-700 px-4 py-2 text-left text-sm text-white'>
-              <Camera className='shrink-0' size='20' /> {caption}
-            </div>
-          )}
-        </div>
-      </Container>
-    </div>
-  );
+	const { image, caption } = blok;
+	const dimensions = getImageDimensionsFromUrl(image.filename);
+	if (!image) return;
+	return (
+		<div className="theme--light bg-background py-8 text-foreground md:py-12 xl:py-16">
+			<Container className="theme--light text-center">
+				<div className="mx-auto inline-block max-w-5xl overflow-hidden rounded-xl">
+					<Image
+						className="block aspect-[5/3] object-cover"
+						src={image.filename}
+						alt={image.alt || image.title || ""}
+						width={dimensions[0]}
+						height={dimensions[1]}
+					/>
+					{caption && (
+						<div className="flex items-center gap-x-4 bg-purple-700 px-4 py-2 text-left text-sm text-white">
+							<Camera className="shrink-0" size="20" /> {caption}
+						</div>
+					)}
+				</div>
+			</Container>
+		</div>
+	);
 };
 
 export default ImageWysiwyg;
